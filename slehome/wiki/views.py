5e7aeef0d1fc6775fac256c2ae2d4f12931c2521
Hongdan <chungdangogo@gmail.com>
2015-01-12 12:30:05 +0900

    wiki 프로토타입

--- slehome/wiki/views.py
+++ slehome/wiki/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
-
+from django.core.context_processors import csrf
+from django.http import HttpResponseRedirect
+from wiki.models import Page, FilePath
+import datetime
 # Create your views here.
+
+
+def view_page(request, page_name=''):
+	if page_name == '':
+		page_name = 'FrontPage'
+
+	try:
+		page = Page.objects.get(page_name=page_name)
+	except Page.DoesNotExist:
+		return render(request,"create.html",{"page_name":page_name})
+	
+	content = page.content
+	pub_date=page.pub_date
+	c={"page_name":page_name,"content":content,"pub_date":pub_date}	
+	
+	return render(request,"view.html",c)
+
+def edit_page(request,page_name):
+	try:
+		page = Page.objects.get(page_name=page_name)
+		content = page.content
+	except Page.DoesNotExist:
+		content = ""
+	c={"page_name":page_name,"content":content}
+	c.update(csrf(request))
+	return render(request,"edit.html",c)
+
+def save_page(request,page_name):
+	content=request.POST["content"]
+	try:
+		page=Page.objects.get(page_name=page_name)
+		page.content=content
+		page.pub_date=datetime.datetime.now()
+	except Page.DoesNotExist:
+		page = Page(page_name=page_name, content=content, pub_date=datetime.datetime.now())
+	page.save()
+	return HttpResponseRedirect("/sle/wiki/"+page_name+"/")
